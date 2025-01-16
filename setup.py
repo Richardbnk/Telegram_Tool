1127ced02b06f3fc438d1593128dd621987d264e
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2025-01-16 00:18:47 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name='Telegram_Tool',
+    version='1.0.47',
+    author="Richard Raphael Banak",
+    description="Robot Specialist Library for Robot Process Automation",
+    url="https://github.com/Richardbnk/Telegram_Tool",
+    packages=['telegram_tool'],
+    
+    py_modules = ['telegram'],
+    
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[required],
+)
