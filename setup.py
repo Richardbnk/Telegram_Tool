ced890a22ea48d886db9cf3945c634aa859fcfe1
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2025-01-16 00:24:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='Telegram_Tool',
-    version='1.0.47',
+    version='1.0.0',
     author="Richard Raphael Banak",
     description="Robot Specialist Library for Robot Process Automation",
     url="https://github.com/Richardbnk/Telegram_Tool",
